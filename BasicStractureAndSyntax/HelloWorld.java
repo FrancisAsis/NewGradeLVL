1456f89a32746d0559bc5f8b47d86f35ead0f7a0
FrancisAsis <francisronelicayan@gmail.com>
2026-06-17 00:02:53 -0500

    First Commit?

---
+++ BasicStractureAndSyntax/HelloWorld.java
@@ -0,0 +1,12 @@
+package BasicStractureAndSyntax;
+ // This is a simple Java program that prints "Hello, World!" to the console.
+
+public class HelloWorld { // The class name is HelloWorld, and it must match the filename (HelloWorld.java).
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("I'm Francisronel I. Asis");
+        System.out.println("I am 17 years old");
+        System.out.println("I live in San Jose Delmonte City");
+        System.out.println("I am a student of Grade 12 Peridot");
+    }
+}
