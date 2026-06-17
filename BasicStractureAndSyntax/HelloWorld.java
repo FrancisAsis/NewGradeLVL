fb56def0cdb1b3dde32ac92bc53d5cad88db9a22
FrancisAsis <francisronelicayan@gmail.com>
2026-06-17 01:32:53 -0500

    FEW ADDDED

--- BasicStractureAndSyntax/HelloWorld.java
+++ BasicStractureAndSyntax/HelloWorld.java
@@ -8,5 +8,19 @@ public class HelloWorld { // The class name is HelloWorld, and it must match the
         System.out.println("I am 17 years old");
         System.out.println("I live in San Jose Delmonte City");
         System.out.println("I am a student of Grade 12 Peridot");
+        System.out.println(136634150273L );
+        System.out.println("Addition" ); // This line prints "Addition" to the console.
+        System.out.print("5 + 3 = " );
+        System.out.println(5 + 3);
+        System.out.println("Subtraction" ); // This line prints "Subtraction" to the console.
+        System.out.print("5 - 3 = ");
+        System.out.println(5 - 3);
+        System.out.println("Multiplication" );// This line prints "Multiplication" to the console.
+        System.out.print("5 * 3 = ");
+        System.out.println(5 * 3);
+        System.out.println("Division" ); // This line prints "Division" to the console.
+        System.out.print("5 / 3 = ");
+        System.out.println(5 / 3);
+
     }
 }
