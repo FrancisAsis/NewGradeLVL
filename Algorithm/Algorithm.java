77f236187cef259ab2143ff7b44b89cbbf32bd40
FrancisAsis <francisronelicayan@gmail.com>
2026-06-25 01:27:40 -0500

    algorithm

---
+++ Algorithm/Algorithm.java
@@ -0,0 +1,23 @@
+package Algorithm;
+
+
+import java.util.Scanner;
+
+public class Algorithm {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = 12;
+
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+    }        
+    input.close();
+    
+    }
+
+
+}
